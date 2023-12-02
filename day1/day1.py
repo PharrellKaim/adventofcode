b60008189dc73fad630bb29d72dfcc882a4ee9d7
Pharrell Kaim <79710229+PharrellKaim@users.noreply.github.com>
2023-12-02 08:55:08 +0100

    solved day1

--- day1/day1.py
+++ day1/day1.py
@@ -1,18 +1,39 @@
-import re
-def extract_numbers_from_string(s):
-    return [int(i) for i in s if i.isdigit()]
+from aocd import get_data
 
 
-file = open("inputDay1", "r")
-summe = int(0)
-zahl = str()
+class Solution:
+    def __init__(self):
+        self.data = get_data(year=2023, day=1).splitlines()
+        self.text_to_number_mapping = {'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6',
+                                       'seven': '7', 'eight': '8', 'nine': '9'}
 
-for line in file:
-    numbers = extract_numbers_from_string(line)
-    print(numbers)
-    for i in numbers:
-        zahl += str(i)
-        print(zahl)
-    summe += int(zahl)
+    def get_number(self, line, reverse=False, digits_only=True):
+        text = ''
+        line_to_check = line[::-1] if reverse else line
 
-print(summe)
+        for x in line_to_check:
+            if x.isdigit():
+                return x
+            if not digits_only:
+                text += x
+
+                for word in self.text_to_number_mapping:
+                    check = word[::-1] if reverse else word
+                    if check in text:
+                        return self.text_to_number_mapping[word]
+
+    def solve(self, digits_only=True):
+        numbers = []
+
+        for line in self.data:
+            first = self.get_number(line, digits_only=digits_only)
+            last = self.get_number(line, True, digits_only=digits_only)
+            numbers.append(int(first + last))
+
+        return sum(numbers)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(f'Part 1: {solution.solve()}')
+    print(f'Part 2: {solution.solve(False)}')
