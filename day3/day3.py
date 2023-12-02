971cb49a77bb44384fc6b2f8f38887a95755427c
Pharrell Kaim <79710229+PharrellKaim@users.noreply.github.com>
2023-12-02 10:11:25 +0100

    created env for day3

---
+++ day3/day3.py
@@ -0,0 +1,21 @@
+from aocd import get_data
+
+class Solution:
+    def __init__(self):
+        #gets input from year 2023 and day 3
+        data = get_data(year=2023, day=3).splitlines()
+
+    #part 1
+    def part1(self):
+        print("nothing to see here until now")
+
+    #part 2
+    def part2(self):
+        print("also nothing to see here until now")
+
+
+
+
+if __name__ == '__main__':
+    #creating an instance of the solution class
+    solution = Solution()
