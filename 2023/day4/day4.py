ba1bd1d87b11cd4e10f174637b8cf77b0d754025
Pharrell Kaim <79710229+PharrellKaim@users.noreply.github.com>
2023-12-08 17:06:30 +0100

    day5

--- 2023/day4/day4.py
+++ 2023/day4/day4.py
@@ -1,36 +1,40 @@
 import re
 from math import pow
 from pathlib import Path
+from aocd import get_data
 
 SCRIPT_PATH = Path(__file__).resolve().parent
 INPUT_PATH = Path(SCRIPT_PATH, "inputDay4")
 
 
 # INPUT_PATH = Path(SCRIPT_PATH, "inputs/example1.txt")
+class Solution:
+    def __init__(self):
+        self.data = get_data(year=2023, day=3).splitlines()
 
-def parse_input(data):
-    # Split the input into lines, remove the card identifier, split by number types, and then separate each individual number
-    cards = [[re.split(r'\s+', part.strip()) for part in row.split(': ')[1].split(' | ')] for row in data.splitlines()]
-    return cards
-
-
-def part1(parsed_data):
-    # Compare the numbers in the first section to the numbers in the second and return 2 to the power of the number of cards won minus one
-    total_value = 0
-    for card_number, card in enumerate(parsed_data):
-        won = 0
-        print(f"Card {card_number + 1} had the following winning numbers:")
-        for number in card[0]:
-            if number in card[1]:
-                won += 1
-                print(number)
-        card_value = pow(2, (won - 1)) if won > 0 else 0  # Ensures that you aren't getting a negative exponent
-        total_value += card_value
-        print("Card {} had a value of {:.0f}".format((card_number + 1), card_value))
-    return total_value
+    def parse_input(data):
+        # Split the input into lines, remove the card identifier, split by number types, and then separate each individual number
+        cards = [[re.split(r'\s+', part.strip()) for part in row.split(': ')[1].split(' | ')] for row in data.splitlines()]
+        return cards
 
 
-def part2(parsed_data):
+    def part1(self, parsed_data):
+        # Compare the numbers in the first section to the numbers in the second and return 2 to the power of the number of cards won minus one
+        total_value = 0
+        for card_number, card in enumerate(parsed_data):
+            won = 0
+            print(f"Card {card_number + 1} had the following winning numbers:")
+            for number in card[0]:
+                if number in card[1]:
+                    won += 1
+                    print(number)
+            card_value = pow(2, (won - 1)) if won > 0 else 0  # Ensures that you aren't getting a negative exponent
+            total_value += card_value
+            print("Card {} had a value of {:.0f}".format((card_number + 1), card_value))
+        return total_value
+
+
+def part2(self, parsed_data):
     # Track how many cards are added at which position, and then return total number of cards
     number_of_cards = [1 for _ in range(len(parsed_data))]  # All cards start with one
     for card_number, card in enumerate(parsed_data):
@@ -46,14 +50,14 @@ def part2(parsed_data):
 
 
 if __name__ == "__main__":
-    with open(INPUT_PATH, "r") as f:
-        parsed_data = parse_input(f.read())
+    solution = Solution()
+    parsed_data = solution.data
 
     print("--- PART 1 ---")
-    answer1 = part1(parsed_data)
+    answer1 = solution.part1(parsed_data)
 
     print("\n--- PART 2 ---")
-    answer2 = part2(parsed_data)
+    answer2 = solution.part2(parsed_data)
 
     print("\n--- ANSWERS ---")
     print("PART1 - The total point value of the cards is {:.0f}".format(answer1))
